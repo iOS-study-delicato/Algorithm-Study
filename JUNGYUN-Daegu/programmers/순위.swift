95662a6f26ffcbf600d940c680022876998939d7
JO JUNGYUN <66199062+JUNGYUN-Daegu@users.noreply.github.com>
2022-01-18 17:06:35 +0900

    Update 순위.swift

--- JUNGYUN-Daegu/programmers/순위.swift
+++ JUNGYUN-Daegu/programmers/순위.swift
@@ -2,12 +2,10 @@
 // https://programmers.co.kr/learn/courses/30/lessons/49191
 // 22.01.18
 
-
 import Foundation
 
 func solution(_ n:Int, _ results:[[Int]]) -> Int {
     
-    // graph는 전적의 여부, 이 후에 모든 사람에 대해 갱신하므로 만약 빠진 전적이 없으면 false로 남게된다.
     var graph = Array(repeating: Array(repeating: false, count: n + 1), count: n + 1)
     
     for result in results {
@@ -31,10 +29,10 @@ func solution(_ n:Int, _ results:[[Int]]) -> Int {
     
     var result: Int = 0
     
-    for a in 1..<n+1 {
+    for a in 1..<n + 1 {
         var count: Int = 0
         
-        for b in 1..<n+1 {
+        for b in 1..<n + 1 {
             if graph[a][b] == true || graph[b][a] == true {
                 count += 1
             }
