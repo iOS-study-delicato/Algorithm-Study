4931e8f678f5f0f37aa6b29e14593fdc725e4b62
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2022-02-14 16:32:43 +0900

    우선 순위 큐 구현

---
+++ JUNGYUN-Daegu/others/우선순위큐.swift
@@ -0,0 +1,118 @@
+
+class PriorityQueue<T: Comparable> {
+    var heap: [T] = []
+    private let comparing: (_ upper: T, _ lower: T) -> Bool
+    
+    init(comparing: @escaping (_ upper: T, _ lower: T) -> Bool) {
+        self.comparing = comparing
+    }
+    
+    func peek() -> T? {
+        return heap.first
+    }
+    
+    func push(_ input: T) {
+        heap.append(input)
+        var new = heap.count - 1
+        var parent = (new - 1) / 2
+        while new > 0 && comparing(heap[new], heap[parent]) {
+            heap.swapAt(new, parent)
+            new = parent
+            parent = (new - 1) / 2
+        }
+    }
+    
+    func pop() -> T? {
+        guard !heap.isEmpty else { return nil }
+        
+        if heap.count == 1 {
+            return heap.removeFirst()
+        }
+        
+        // result is first element of heap. The element will be swapped with the last element, removed from heap at the end
+        let result = heap.first
+        heap.swapAt(0, heap.count - 1)
+        heap.removeLast()
+        
+        var parent = 0
+        
+        // move down parent(if smaller than a child)
+        while parent < heap.count {
+            let left = (parent * 2) + 1
+            let right = left + 1
+            
+            if right < heap.count {
+                if comparing(heap[right], heap[left]) {
+                    if comparing(heap[right], heap[parent]) {
+                        heap.swapAt(right, parent)
+                        parent = right
+                    } else if comparing(heap[left], heap[parent]){
+                        heap.swapAt(left, parent)
+                        parent = left
+                    } else {
+                        break
+                    }
+                } else {
+                    if comparing(heap[left], heap[parent]) {
+                        heap.swapAt(left, parent)
+                        parent = left
+                    } else if comparing(heap[right], heap[parent]) {
+                        heap.swapAt(right, parent)
+                        parent = right
+                    } else {
+                        break
+                    }
+                }
+            } else if left < heap.count {
+                if comparing(heap[left], heap[parent]) {
+                    heap.swapAt(left, parent)
+                    parent = left
+                } else {
+                    break
+                }
+            } else {
+                break
+            }
+        }
+        
+        return result
+    }
+}
+
+var pq = PriorityQueue<Int> { (upper, lower) -> Bool in
+    upper > lower
+}
+
+let N = Int(readLine()!)!
+var result = [Int]()
+for _ in 0..<N {
+    let input = Int(readLine()!)!
+
+    if input == 0 {
+        result.append(pq.pop() ?? 0)
+    } else {
+        pq.push(input)
+    }
+}
+
+for element in result {
+    print(element)
+}
+
+/*
+ Test Case 1
+ 13
+ 0
+ 1
+ 2
+ 0
+ 0
+ 3
+ 2
+ 1
+ 0
+ 0
+ 0
+ 0
+ 0
+ */
